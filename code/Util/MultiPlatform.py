85dfb598c1afead49e964ef1a1033274c6bab0e7
Lidi <peru12003@gmail.com>
2024-10-16 11:56:03 +0300

    GUY's use os.path.sep instead of code

--- code/Util/MultiPlatform.py
+++ code/Util/MultiPlatform.py
@@ -3,18 +3,19 @@ import platform
 
 
 def get_path_separator():
-        system = platform.system()
-        if system == 'Linux' or system == 'Darwin':  # Darwin is macOS
-            separator = '/'
-        elif system == 'Windows':
-            separator = '\\'
-        else:
-            raise EnvironmentError("Unsupported operating system")
+        return os.path.sep
+        # system = platform.system()
+        # if system == 'Linux' or system == 'Darwin':  # Darwin is macOS
+        #     separator = '/'
+        # elif system == 'Windows':
+        #     separator = '\\'
+        # else:
+        #     raise EnvironmentError("Unsupported operating system")
         
-        print(f"Running on {system}. Path separator: {separator}")
-        return separator
+        # print(f"Running on {system}. Path separator: {separator}")
+        # return separator
     
-def get_home_directory():
+def get_home_directory():    
     system = platform.system()
     if system == 'Linux' or system == 'Darwin':  # Darwin is macOS
         home_dir = os.environ.get('HOME')        
