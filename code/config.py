f1afbcc58188caab455fbea8dbc372ad7287d844
Lidi <peru12003@gmail.com>
2024-10-27 14:15:51 +0200

    added configuration visible globally in code/config.py, added missing __init__.py files

---
+++ code/config.py
@@ -0,0 +1,7 @@
+
+
+current_model_name = "resnet50"
+#current_model_name = "resnet152"
+current_model_name = current_model_name.lower()
+split_data_dataloaders = True
+testset_evaluation_if_exists = False
